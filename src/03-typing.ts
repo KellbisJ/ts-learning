ac5573119a6347b7ba0705ea29a443942da7baa5
Kellbis <holasoykellbis@gmail.com>
2025-01-09 08:14:16 -0400

    basic typing

---
+++ src/03-typing.ts
@@ -0,0 +1,11 @@
+(() => {
+  let product_name = '1';
+  let product_price = 17;
+  let cart = [];
+
+  product_name.toLowerCase(); // only returns string methods when you put . after variable name reference
+  product_price.valueOf(); // only returns number methods when you put . after variable name reference
+  cart.length; // only returns array methods when you put . after variable name reference
+
+  const product_stock = 1000;
+})();
