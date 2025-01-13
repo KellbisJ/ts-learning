87add1c6c97823a663f197ad5349b9fda262f179
Kellbis <holasoykellbis@gmail.com>
2025-01-13 15:43:09 -0400

    class

---
+++ src/poo_and_asynchronism/01-class.ts
@@ -0,0 +1,16 @@
+(() => {
+  class MyDate {
+    year: number;
+    month: number;
+    day: number;
+
+    constructor(year: number, month: number, day: number) {
+      this.year = year;
+      this.month = month;
+      this.day = day;
+    }
+  }
+
+  const myDate = new MyDate(2006, 1, 21);
+  console.log(myDate);
+})();
